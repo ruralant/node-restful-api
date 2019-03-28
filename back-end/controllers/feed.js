01ca0e05a40ee32e6ca6ce5130d7a7c03094b34d
Antonio Rossi <info@antoniorossi.net>
2019-03-28 21:00:14 +0000

    get and post messages working

--- back-end/controllers/feed.js
+++ back-end/controllers/feed.js
@@ -1,41 +1,62 @@
 const { validationResult } = require('express-validator/check');
 
-exports.getPosts = (req, res, next) => {
-  // dummy data for the moment
-  res.status(200).json({
-    posts: [
-      { 
-        _id: '1',
-        title: 'First Post', 
-        content: 'This is the first post!',
-        imageUrl: 'images/wolf.js',
-        creator: {
-          name: 'Antonio'
-        },
-        createdAt: new Date()
-      }
-    ]
-  });
-};
+const Post = require('../models/post');
 
-exports.createPost = (req, res, next) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ message: 'Validation failed ', errors: errors.array() });
-  }
-
-  const { title } = req.body;
-  const { content } = req.body;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { 
-      _id: new Date().toISOString(), 
-      title, 
-      content, 
-      creator: { name: 'Antonio' },
-      createdAt: new Date()
+exports.getPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json({ message: 'Posts fetched successfully', posts });
+  } catch (e) {
+    if (!e.statusCode) {
+      e.statusCode = 500;
     }
-  });
+    next(e);
+  }
 };
+
+exports.createPost = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+  
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation failed ');
+      error.statusCode = 422;
+      throw error;
+    }
+
+    const { title } = req.body;
+    const { content } = req.body;
+    let post = new Post({
+      title,
+      content,
+      imageUrl: 'images/wolf.jpg', //for now 
+      creator: { name: 'Antonio' },
+    })
+      post = await post.save();
+      res.status(201).json({ message: 'Post created successfully!', post });
+  } catch (e) {
+    if (!e.statusCode) {
+      e.statusCode = 500;
+    }
+    next(e);
+  }
+};
+
+exports.getPost = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+
+    if (!post) {
+      const error = new Error('No post found');
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({ message: 'Post fetched', post });
+  } catch (e) {
+    if (!e.statusCode) {
+      e.statusCode = 500;
+    }
+    next(e);
+  }
+}
--- back-end/routes/feed.js
+++ back-end/routes/feed.js
@@ -10,8 +10,10 @@ router.get('/posts', feedController.getPosts);
 
 // POST /feed/post
 router.post('/post', [
-    body('title').trim().isLength({ min: 5 }),
-    body('creator').trim().isLength({ min: 5 }),
+    body('title').trim().isLength({ min: 10 }),
+    body('content').trim().isLength({ min: 5 }),
 ], feedController.createPost);
+
+router.get('/post/:id', feedController.getPost);
 
 module.exports = router;
